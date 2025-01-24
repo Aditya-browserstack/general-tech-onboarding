6bc08749709e8e1c397ecedef2e0b5a9cc9ab529
Aditya Shekhawat <aditya.sh@browserstack.com>
2025-01-24 15:47:24 +0530

    Sorting on the basis of frequency(descendin)

--- script.sh
+++ script.sh
@@ -4,5 +4,5 @@ grep 'Completed' ./tmp_more_than_50 | sort -nk7 | tr -d '"' | tail -1
 
 echo ""
 echo "The list of requests with their frequency are :"
-grep 'Started' ./tmp_more_than_50 | cut -d' ' -f3 | cut -d'?' -f1 | tr -d '"'| sort | uniq -c | awk '{print $2,$1}'
+grep 'Started' ./tmp_more_than_50 | cut -d' ' -f3 | cut -d'?' -f1 | tr -d '"'| sort | uniq -c | sort -rn |awk '{print $2,$1}'
 
