dd02580a671c8672db44f01a68a6ff3fc0a065de
Aditya Shekhawat <aditya.sh@browserstack.com>
2025-01-24 15:39:07 +0530

    Changed the endpoint format as no quotes

--- script.sh
+++ script.sh
@@ -1,8 +1,8 @@
 echo ""
 echo "The request that took the longest time is :"
-grep 'Completed' ./tmp_more_than_50 | sort -nk7 | tail -1
+grep 'Completed' ./tmp_more_than_50 | sort -nk7 | tr -d '"' | tail -1
 
 echo ""
 echo "The list of requests with their frequency are :"
-grep 'Started' ./tmp_more_than_50 | cut -d' ' -f3 | cut -d'?' -f1 | sort | uniq -c | awk '{print $2,$1}'
+grep 'Started' ./tmp_more_than_50 | cut -d' ' -f3 | cut -d'?' -f1 | tr -d '"'| sort | uniq -c | awk '{print $2,$1}'
 
